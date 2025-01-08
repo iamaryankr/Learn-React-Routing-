dbba6ae60324a6ad49d550fc7afc802f592290f7
iamaryankr <95643709+iamaryankr@users.noreply.github.com>
2025-01-09 02:48:55 +0530

    new project

---
+++ src/components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+function About(){
+    return(
+        <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.gforgames.com%2Fwp-content%2Fuploads%2F2020%2F08%2FIT-sector.jpg&f=1&nofb=1&ipt=e7750e97634d56d35fcf412ca155d78819457132fadb7beb7b43d60167715631&ipo=images"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            React development is carried out by passionate developers
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
